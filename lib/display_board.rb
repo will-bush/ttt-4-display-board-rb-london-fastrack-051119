09ebecc7e9a73962e16d100eeb945f5e1d40ea24
William Bush <williamdcbush@gmail.com>
2019-05-11 17:30:29 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,60 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+column = "|"
+row = "-----------"
+
+puts " #{board[0]} #{column} #{board[1]} #{column} #{board[2]} "
+puts "#{row}"
+puts " #{board[3]} #{column} #{board[4]} #{column} #{board[5]} "
+puts "#{row}"
+puts " #{board[6]} #{column} #{board[7]} #{column} #{board[8]} "
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(board)
+
+puts "new board"
+
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+display_board(board)
+
+puts "new board"
+
+board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(board)
+
+puts "new board"
+
+board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+display_board(board)
+
+puts "new board"
+
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+display_board(board)
+
+puts "new board"
+
+board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+display_board(board)
+
+puts "new board"
+
+board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+display_board(board)
+
+puts "new board"
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(board)
+
+puts "new board"
+
+board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+display_board(board)
+
+puts "new board"
+
+board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+display_board(board)
